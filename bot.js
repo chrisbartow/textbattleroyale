f3bf55f25be1f475f150b5184e55b60cba35ea63
chrisbartow <cbartow@gmail.com>
2019-08-04 11:07:02 -0400

    Added bots, dancing, and leaderboard updates.

--- bot.js
+++ bot.js
@@ -42,9 +42,6 @@ var gameState = "lobby";
 // Create an array of players
 var players = new Array();
 
-// Add some fake players for now
-players.push({ 'id': 100, 'name': 'Rick', 'xp': 0, 'wins': 0, 'kills': 0, 'games': 0 }, { 'id': 200, 'name': 'Morty', 'xp': 0, 'wins': 0, 'kills': 0, 'games': 0 });
-
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
     console.log(`Connected to ${addr}:${port}`);
@@ -56,17 +53,17 @@ function onConnectedHandler(addr, port) {
 function onMessageHandler(target, context, msg, self) {
     if (self) { return; } // Ignore messages from the bot
 
-    console.log(target, '<' + context['username'] + '>', msg);
+    console.log(target, '<' + context['display-name'] + '>', msg);
     const cmd = msg.trim();
 
     // !join the game
-    if (/^!join/.test(cmd) && gameState === 'lobby') {
+    if (/^!join/i.test(cmd) && gameState === 'lobby') {
         // Make sure they haven't joined yet
         if (players.findIndex(x => x.id === Number(context['user-id'])) === -1) {
 
             var playerObj = {
                 "id": Number(context['user-id']),
-                "name": context['username'],
+                "name": context['display-name'],
                 "xp": 0,
                 "wins": 0,
                 "games": 0,
@@ -81,14 +78,52 @@ function onMessageHandler(target, context, msg, self) {
                 }
 
                 players.push(playerObj);
-
-                client.say(target, `${playerObj['name']} joined the battle!`);
+                if (players.length === 1) {
+                    client.say(target, `${playerObj['name']} started a new lobby! Type !join to fight for that cheesecake.`);
+                }
             });
         }
     }
 
+    if (/^!bots/i.test(cmd) && gameState === 'lobby') {
+        var bots = cmd.match(/^!bots *([0-9\-_]*)/i)[1];
+
+        if (!bots)
+            bots = Math.floor(Math.random() * 10) + 1;
+
+        var playerName, playerId;
+        for (var i = 0; i < bots; i++) {
+            if (Math.floor((Math.random() * 2))) {
+                playerName = "Rick";
+                playerId = "R" + Math.floor(Math.random() * 1000);
+            } else {
+                playerName = "Morty";
+                playerId = "M" + Math.floor(Math.random() * 1000);
+            }
+            if (players.findIndex(x => x.id === playerId === -1)) {
+
+                var playerObj = {
+                    "id": playerId,
+                    "name": playerName,
+                    "xp": 0,
+                    "wins": 0,
+                    "games": 0,
+                    "kills": 0
+                };
+
+                players.push(playerObj);
+            } else {
+                bots--;
+            }
+        }
+
+        if (players.length === Number(bots)) {
+            client.say(target, `${bots} robots have started a new lobby! Type !join to prevent them from getting the cheesecake.`);
+        }
+    }
+
     // !drop Start the game
-    if (/^!drop/.test(cmd) && gameState === 'lobby') {
+    if (/^!drop/i.test(cmd) && gameState === 'lobby') {
         if (players.length > 1) {
             gameState = "active";
             client.say(target, "The battle begins in 10 seconds... ");
@@ -99,31 +134,51 @@ function onMessageHandler(target, context, msg, self) {
     }
 
     // !br show game info
-    if (/^!br/.test(cmd)) {
+    if (/^!br/i.test(cmd)) {
         client.say(target, `Text BR is a text based version of your favorite last man or woman standing game. Type !join to grab a spot in the next game.`);
     }
 
+    // !dance monkey dance
+    if (/^!dance/i.test(cmd)) {
+        var result = cmd.match(/^!dance *([a-z0-9\-_]*)/i);
+        if (result[1]) {
+            client.say(target, `${context['display-name']} dances with ${result[1]}.`);
+        } else {
+            client.say(target, `${context['display-name']} dances with themselves.`);
+        }
+    }
+
+    // !lobby show players in the lobby
+    if (/^!lobby/i.test(cmd)) {
+        if (players.length) {
+            client.say(target, `The following players are waiting impatiently in the lobby: ` + players.map(p => p.name).join(', '));
+        } else {
+            client.say(target, `No one is in the lobby. Type !join to enter.`);
+        }
+    }
+
     // !top show top players
-    if (/^!top/.test(cmd)) {
+    if (/^!rank/i.test(cmd)) {
         db.all(`SELECT name, xp FROM players ORDER BY xp DESC LIMIT 10`, [], (err, rows) => {
             if (err) throw err;
-            client.say(target, "The top players of Text Battle Royale are: " + rows.map(player => `${player.name} (${player.xp})`).join(', '));
+            client.say(target, "The top ranked players of Text Battle Royale by XP are: " + rows.map(player => `${player.name} (${player.xp})`).join(', '));
         });
     }
 
     // !stats show stats for player
-    if (/^!stats/.test(cmd)) {
-        var result = cmd.match(/^!stats *([A-z0-9\-_]*)/);
+    if (/^!stats/i.test(cmd)) {
+        var result = cmd.match(/^!stats *([A-z0-9\-_]*)/i);
 
         if (result[1])
             var username = result[1];
         else
-            var username = context['username'];
+            var username = context['display-name'];
 
         db.all(`SELECT * FROM players WHERE name = ? COLLATE NOCASE`, [username], (err, rows) => {
             if (err) throw err;
             if (rows.length) {
-                client.say(target, `${rows[0]['name']} has earned ${rows[0]['xp']} XP playing in ${rows[0]['games']} games with ${rows[0]['kills']} kills and ${rows[0]['wins']} wins.`);
+                var kdratio = rows[0]['kills'] / (rows[0]['games'] - rows[0]['wins']);
+                client.say(target, `${rows[0]['name']} has earned ${rows[0]['xp']} XP playing in ${rows[0]['games']} games with ${rows[0]['kills']} kills, ${rows[0]['wins']} wins, and K/D Ratio of ${kdratio.toFixed(2)}.`);
             } else {
                 client.say(target, `Can't find user ${username}.`);
             }
@@ -185,8 +240,12 @@ function battleroyale(target, notdeadyet) {
 
         // Write out Player Stats to Database
         players.forEach(function(p) {
-            // TODO: Convert into one Replace statement Array of Array [[],[]]
+            // If it's a bot, don't log it.
+            if (isNaN(p.id))
+                return;
             p.games++;
+
+            // TODO: Convert into one Replace statement Array of Array [[],[]]
             db.run(`REPLACE INTO players(id, name, xp, wins, games, kills) VALUES(?, ?, ?, ?, ?, ?)`, [p.id, p.name, p.xp, p.wins, p.games, p.kills], function(err) {
                 if (err) throw err;
             });
@@ -204,6 +263,8 @@ function OverlayServer() {
     var fs = require('fs');
     var http = require('http');
     var path = require('path');
+
+    console.log("HTTP Overlay Server running.");
 
     http.createServer(function(req, res) {
         let url = path.normalize(req.url).replace(/^\//, "");
@@ -226,7 +287,30 @@ function OverlayServer() {
                             /%LEADERBOARD%/g,
                             rows.map(
                                 (player, index) =>
-                                `<li>${index+1}. ${player['name']} <span class="total">${player[url]}</span></li>`).join("")
+                                `<li><div class="player">${index+1}. ${player['name']}</div> <div class="stat">${player[url]}</div></li>`).join("")
+                        );
+                        res.write(html);
+                        res.end();
+                    }
+                });
+
+            });
+        } else if (url === "kills") {
+            fs.readFile(`overlay/kills-leaders.html`, 'utf8', async function(err, html) {
+                if (err) {
+                    return console.log(err);
+                }
+                // Create HTML Overlay for Top players
+
+                db.all(`SELECT name, kills, round(CAST(kills AS FLOAT)/(games-wins),2) as KD FROM players ORDER BY kills DESC LIMIT 10`, [], (err, rows) => {
+                    if (err) return err;
+                    if (rows.length) {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        html = html.replace(
+                            /%LEADERBOARD%/g,
+                            rows.map(
+                                (player, index) =>
+                                `<li><div class="player">${index+1}. ${player['name']}</div> <div class="stat">${player['kills']}</div> <div class="stat">${player['KD']}</div></li>`).join("")
                         );
                         res.write(html);
                         res.end();
